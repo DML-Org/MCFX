3c006e11bbc92b6c37df0814d8981eb2d6cc55f9
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-11-28 12:14:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -688,7 +688,7 @@ async def auto_filter(client, msg, spoll=False):
 
     btn.insert(0,
         [
-            InlineKeyboardButton(text="⚡How To Download⚡", url='https://t.me/How_to_Download_From_Search_Bot/2')
+            InlineKeyboardButton(text="⚡How To Download⚡", url='https://t.me/ClickAdd_MCF')
         ]
     )
 
