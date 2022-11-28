0153cd2603fb90b2247ef80f307c5edf27081304
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-11-28 10:42:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,9 +42,20 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'MovieClubFamily_Chat')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{file_name}</code> \n\nᴊᴏɪɴ ɴᴏᴡ: [MCF FilterX Bot](https://t.me/MovieClubFamily)</b>")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{file_name}</code> \n\nᴊᴏɪɴ ɴᴏᴡ: [MCF FilterX Bot](https://t.me/MovieClubFamily)</b>")
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🧿 ᴛɪᴛᴛʟᴇ :  {title} \n🌟 ʀᴀᴛɪɴɢ : {rating} \n🎭 ɢᴇɴʀᴇ : {genres} \n📆 ʀᴇʟᴇᴀsᴇ : {year} \n⏰ ᴅᴜʀᴀᴛɪᴏɴ : {runtime} \n🎙️ʟᴀɴɢᴜᴀɢᴇ : {languages} \n🔖 sʜᴏʀᴛ : {plot} \n★ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : @GreyMatter_Bots")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{file_name}</code> \n\nJoin Now: [MCF FilterX Bot](https://t.me/MovieClubFamily)</b>")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "ɴᴀᴍᴇ: <code>{file_name}</code> \n\nJoin Now: [MCF FilterX Bot](https://t.me/MovieClubFamily)</b>")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "🏷 Title: {title}
+🎭 Genres: {genres}
+📆 Year: {year}
+🌟 Rating: {rating}/ 10 (based on {votes} user ratings.)
+☀️ Languages : {languages}
+📀 RunTime: {runtime} Minutes
+📆 Release Info : {release_date}
+🎛 Countries : {countries}
+
+┏━━━━•❅•°•❈•°•❅•━━━━┓
+   🇱🇰𝐌𝐨𝐯𝐢𝐞𝐂𝐥𝐮𝐛 𝐅𝐚𝐦𝐢𝐥𝐲™🎭
+┗━━━━•❅•°•❈•°•❅•━━━━┛")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "False"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
